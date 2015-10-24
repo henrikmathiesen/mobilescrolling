466a3c414ed895cb075734f66fd4f941a4203eab
Henrik <henrik_mathiesen@hotmail.com>
2015-10-24 12:22:06 +0200

    -sticky behavior on scroll direction upwards

--- js/jQueryScrolling.js
+++ js/jQueryScrolling.js
@@ -1,13 +1,22 @@
 (function () {
+	
+	/*
+		TWO VARIANTS: toggle sticky header on coord or on scroll direction upwards
+		- With debounce (sticky behavior happens N ms after scroll event is done), for perfomance reasons
+		- The y coordinate seems to update fine on iOS, even when accelerated swipe
+		- When rubber scrolling on iOS, above top is a negative y coord
+		- Dont mess with padding on body during animation, it will be jerky on iOS
+		- Observe the css classes, their position states
+		- Use CSS animations (not transition, not jQuery animate)
+	*/
+	
 
-	var $body = $('body');
 	var $debugBar = $('.debug-bar');
+	var lastScrollTop = 0;
 
-
-	// Returns a function, that, as long as it continues to be invoked, will not
-	// be triggered. The function will be called after it stops being called for
-	// N milliseconds. If `immediate` is passed, trigger the function on the
-	// leading edge, instead of the trailing.
+	// Returns a function, that, as long as it continues to be invoked, will not be triggered.
+	// The function will be run after it stops being called for N milliseconds.
+	// If `immediate` is passed, trigger the function on the leading edge, instead of the trailing.
 	function debounce(func, wait, immediate) {
 		var timeout;
 		return function () {
@@ -22,7 +31,6 @@
 			if (callNow) func.apply(context, args);
 		};
 	};
-
 
 
 	var stickyHeader = debounce(function (e) {
@@ -50,21 +58,31 @@
 	}, 250);
 
 
+	var stickyHeaderOnScrollUpwards = debounce(function (e) {
 
+		var scrollY = e.currentTarget.scrollY;
 
+		$debugBar.text(scrollY);
+		
+		if(scrollY < 40) {
+			$debugBar.removeClass('sticky unsticky');
+		}
+		else if (scrollY > lastScrollTop) {
+			// USER SCROLLING DOWN
+			if ($debugBar.hasClass('sticky')) {
+				$debugBar.addClass('unsticky');
+			}
+		}
+		else {
+			// USER SCROLLING UP
+			$debugBar.removeClass('unsticky');
+			$debugBar.addClass('sticky');
+		}
 
+		lastScrollTop = scrollY;
 
+	}, 250);
 
-	$(window).on('scroll', stickyHeader);
-
+	$(window).on('scroll', stickyHeaderOnScrollUpwards);
+	
 })();
-
-
-/*
-
-	- The y coordinate seems to update fine on iOS, even when accelerated swipe
-	- When rubber scrolling on iOS, above top is a negative y coord
-	- We always want to add padding top to body, equal to size of sticky header, else user loses track of content
-		* But doing so makes jerky behavoir in iOS
-
-*/
